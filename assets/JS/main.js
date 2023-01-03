8bd95b65f1e5557f19be8ab9eec450c7441e350d
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-03 19:10:37 +0400

    feat: add translation

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -11,29 +11,17 @@ let paperDiv = document.getElementById('Paper');
 
 function getComputerChoice() {
     let choices = ['Rock', 'Scissors', 'Paper'];
-    let randomNumber = Math.floor(Math.random() *3);
+    let randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
-}
-
-function convertToRussian(inputed) {
-    if (inputed === 'Rock') {
-        return 'Камень';
-    }      
-    else if (inputed === 'Scissors') {
-        return 'Ножницы';
-    }
-    else if (inputed === 'Paper') {
-        return 'Бумага';
-    }
 }
 
 function win(userChoice, computerChoice) {
     userScore++;
     userScoreSpan.innerHTML = userScore;
     computerScoreSpan.innerHTML = computerScore;
-    const smallUserWord = 'игрок'.fontsize(3).sub();
-    const smallCompWord = 'комп'.fontsize(3).sup();
-    resultsDiv.innerHTML = `${convertToRussian(userChoice)} ${smallUserWord} и ${convertToRussian(computerChoice)} ${smallCompWord}. Вы победили!`;
+    const smallUserWord = 'user'.fontsize(3).sub();
+    const smallCompWord = 'comp'.fontsize(3).sup();
+    resultsDiv.innerHTML = `${userChoice} ${smallUserWord} и ${computerChoice} ${smallCompWord}. You won!`;
     let mySound = new Audio('assets/JS/mixkit-achievement-bell-600.wav');
     mySound.volume = 0.5;
     mySound.play();
@@ -43,9 +31,9 @@ function lose(userChoice, computerChoice) {
     computerScore++;
     userScoreSpan.innerHTML = userScore;
     computerScoreSpan.innerHTML = computerScore;
-    const smallUserWord = 'игрок'.fontsize(3).sub();
-    const smallCompWord = 'комп'.fontsize(3).sup();
-    resultsDiv.innerHTML = `${convertToRussian(userChoice)} ${smallUserWord} и ${convertToRussian(computerChoice)} ${smallCompWord}. Вы проиграли!`;
+    const smallUserWord = 'user'.fontsize(3).sub();
+    const smallCompWord = 'comp'.fontsize(3).sup();
+    resultsDiv.innerHTML = `${userChoice} ${smallUserWord} и ${computerChoice} ${smallCompWord}. You lose!`;
     let mySound = new Audio('assets/JS/mixkit-retro-arcade-lose-2027.wav');
     mySound.volume = 0.5;
     mySound.play();
@@ -54,9 +42,9 @@ function lose(userChoice, computerChoice) {
 function draw(userChoice, computerChoice) {
     userScoreSpan.innerHTML = userScore;
     computerScoreSpan.innerHTML = computerScore;
-    const smallUserWord = 'игрок'.fontsize(3).sub();
-    const smallCompWord = 'комп'.fontsize(3).sup();
-    resultsDiv.innerHTML = `${convertToRussian(userChoice)} ${smallUserWord} и ${convertToRussian(computerChoice)} ${smallCompWord}. Ничья!`;
+    const smallUserWord = 'user'.fontsize(3).sub();
+    const smallCompWord = 'comp'.fontsize(3).sup();
+    resultsDiv.innerHTML = `${userChoice} ${smallUserWord} and ${computerChoice} ${smallCompWord}.draw!`;
     let mySound = new Audio('assets/JS/mixkit-unlock-game-notification-253.wav');
     mySound.volume = 0.5;
     mySound.play();
@@ -85,15 +73,15 @@ function game(userChoice) {
 
 function main() {
 
-    rockDiv.addEventListener('click', function() {
+    rockDiv.addEventListener('click', function () {
         game('Rock');
     })
 
-    scissorsDiv.addEventListener('click', function() {
+    scissorsDiv.addEventListener('click', function () {
         game('Scissors');
     })
 
-    paperDiv.addEventListener('click', function() {
+    paperDiv.addEventListener('click', function () {
         game('Paper');
     })
 
