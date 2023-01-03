aa18474b91a67c5de48a0fdf7ec4b3a530f752c7
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-03 19:19:25 +0400

    refactor: brush up the code

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,53 +1,53 @@
-let userScore = 0;
-let computerScore = 0;
-let userScoreSpan = document.getElementById('userScore');
-let computerScoreSpan = document.getElementById('computerScore');
-let scoreBoardDiv = document.querySelector('.score--board');
-let resultsDiv = document.querySelector('.results > p');
-let rockDiv = document.getElementById('Rock');
-let scissorsDiv = document.getElementById('Scissors');
-let paperDiv = document.getElementById('Paper');
+let userScore = 0
+let computerScore = 0
+let userScoreSpan = document.getElementById('userScore')
+let computerScoreSpan = document.getElementById('computerScore')
+let scoreBoardDiv = document.querySelector('.score--board')
+let resultsDiv = document.querySelector('.results > p')
+let rockDiv = document.getElementById('Rock')
+let scissorsDiv = document.getElementById('Scissors')
+let paperDiv = document.getElementById('Paper')
 
 
 function getComputerChoice() {
-    let choices = ['Rock', 'Scissors', 'Paper'];
-    let randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
+    let choices = ['Rock', 'Scissors', 'Paper']
+    let randomNumber = Math.floor(Math.random() * 3)
+    return choices[randomNumber]
 }
 
 function win(userChoice, computerChoice) {
-    userScore++;
-    userScoreSpan.innerHTML = userScore;
-    computerScoreSpan.innerHTML = computerScore;
-    const smallUserWord = 'user'.fontsize(3).sub();
-    const smallCompWord = 'comp'.fontsize(3).sup();
-    resultsDiv.innerHTML = `${userChoice} ${smallUserWord} and ${computerChoice} ${smallCompWord}. You won!`;
-    let mySound = new Audio('assets/JS/mixkit-achievement-bell-600.wav');
-    mySound.volume = 0.5;
-    mySound.play();
+    userScore++
+    userScoreSpan.innerHTML = userScore
+    computerScoreSpan.innerHTML = computerScore
+    const smallUserWord = 'user'.fontsize(3).sub()
+    const smallCompWord = 'comp'.fontsize(3).sup()
+    resultsDiv.innerHTML = `${userChoice} ${smallUserWord} and ${computerChoice} ${smallCompWord}. You won!`
+    let mySound = new Audio('assets/JS/mixkit-achievement-bell-600.wav')
+    mySound.volume = 0.5
+    mySound.play()
 }
 
 function lose(userChoice, computerChoice) {
-    computerScore++;
-    userScoreSpan.innerHTML = userScore;
-    computerScoreSpan.innerHTML = computerScore;
-    const smallUserWord = 'user'.fontsize(3).sub();
-    const smallCompWord = 'comp'.fontsize(3).sup();
-    resultsDiv.innerHTML = `${userChoice} ${smallUserWord} and ${computerChoice} ${smallCompWord}. You lose!`;
-    let mySound = new Audio('assets/JS/mixkit-retro-arcade-lose-2027.wav');
-    mySound.volume = 0.5;
-    mySound.play();
+    computerScore++
+    userScoreSpan.innerHTML = userScore
+    computerScoreSpan.innerHTML = computerScore
+    const smallUserWord = 'user'.fontsize(3).sub()
+    const smallCompWord = 'comp'.fontsize(3).sup()
+    resultsDiv.innerHTML = `${userChoice} ${smallUserWord} and ${computerChoice} ${smallCompWord}. You lose!`
+    let mySound = new Audio('assets/JS/mixkit-retro-arcade-lose-2027.wav')
+    mySound.volume = 0.5
+    mySound.play()
 }
 
 function draw(userChoice, computerChoice) {
-    userScoreSpan.innerHTML = userScore;
-    computerScoreSpan.innerHTML = computerScore;
-    const smallUserWord = 'user'.fontsize(3).sub();
-    const smallCompWord = 'comp'.fontsize(3).sup();
-    resultsDiv.innerHTML = `${userChoice} ${smallUserWord} and ${computerChoice} ${smallCompWord}.draw!`;
-    let mySound = new Audio('assets/JS/mixkit-unlock-game-notification-253.wav');
-    mySound.volume = 0.5;
-    mySound.play();
+    userScoreSpan.innerHTML = userScore
+    computerScoreSpan.innerHTML = computerScore
+    const smallUserWord = 'user'.fontsize(3).sub()
+    const smallCompWord = 'comp'.fontsize(3).sup()
+    resultsDiv.innerHTML = `${userChoice} ${smallUserWord} and ${computerChoice} ${smallCompWord}.draw!`
+    let mySound = new Audio('assets/JS/mixkit-unlock-game-notification-253.wav')
+    mySound.volume = 0.5
+    mySound.play()
 }
 
 function game(userChoice) {
@@ -57,34 +57,33 @@ function game(userChoice) {
         case 'PaperRock':
         case 'ScissorsPaper':
             win(userChoice, computerChoice);
-            break;
+            break
         case 'ScissorsRock':
         case 'RockPaper':
         case 'PaperScissors':
             lose(userChoice, computerChoice);
-            break;
+            break
         case 'ScissorsScissors':
         case 'PaperPaper':
         case 'RockRock':
-            draw(userChoice, computerChoice);
-            break;
+            draw(userChoice, computerChoice)
+            break
     }
 }
 
 function main() {
 
     rockDiv.addEventListener('click', function () {
-        game('Rock');
+        game('Rock')
     })
 
     scissorsDiv.addEventListener('click', function () {
-        game('Scissors');
+        game('Scissors')
     })
 
     paperDiv.addEventListener('click', function () {
-        game('Paper');
+        game('Paper')
     })
-
 }
 
-main();
+main()
